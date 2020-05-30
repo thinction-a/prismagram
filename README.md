24bf8403f6bae854165d0eb87a3d52da85c7b739
thinction-cmo <jakuro94@gmail.com>
2020-05-30 13:40:03 +0900

    prismagram-back_end finished

--- README.md
+++ README.md
@@ -8,12 +8,18 @@ Instagram Clone with Express + Prisma + React and React Native
 - [x] Confirm Secret (Login)
 - [x] Like  / Unlike a photo
 - [x] Comment on a photo
-- [ ] Search by user
-- [ ] Search by location
-- [ ] See user profile
-- [ ] Follod / Unfollow User
-- [ ] See the full photo
-- [ ] Edit my profile
-- [ ] Upload a photo
-- [ ] Edit the photo (Delete)
-- [ ] See the feed
+- [x] Search by user
+- [x] Search by location
+- [x] Follow User
+- [x] unFollow User
+- [x] Edit my profile
+- [x] See user profile
+- [x] See MY profile
+- [x] See the full photo
+- [x] Upload a photo
+- [x] Edit the photo (Delete)
+- [x] See the feed
+- [x] Send private Message
+- [x] See rooms
+- [x] See room
+- [x] Receive Message (Realtime)
---
+++ src/api/User/me/me.js
@@ -0,0 +1,29 @@
+import { prisma } from "../../../../generated/prisma-client";
+import { USER_FRAGMENT } from "../../../fragment";
+
+
+export default {
+    Query: {
+        me: async (_, __, { request, isAuthenticated }) => {
+            isAuthenticated(request);
+            const { user } = request;
+            const userProfile = await prisma.user({ id: user.id });
+            const posts = await prisma.user({ id: user.id }).posts();
+            return {
+                user: userProfile,
+                posts
+            };
+        }
+    }
+}
+
+
+// export default {
+//     Query: {
+//         me: (_, __, { request, isAuthenticated }) => {
+//             isAuthenticated(request);
+//             const { user } = request;
+//             return prisma.user({ id: user.id }).$fragment(USER_FRAGMENT);
+//         }
+//     }
+// }
